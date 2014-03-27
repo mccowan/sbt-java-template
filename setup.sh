9fda21d4a6a4e1330fb529ec963dab9b1c0dfd73
Mike <michael.mccowan@gmail.com>
2014-03-26 23:25:24 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+P=$(basename $(pwd))
+git clone git@github.com:mccowan/sbt-java-template.git .
+sed -i -e "s/__PROJECT_SLUG__/${P}/g" build.sbt
+rm -rf .git && \
+echo "Successfully initialized ${P} at $(pwd)."
